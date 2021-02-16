284cc6341641b27b9a6967d670f5ffcc15934653
Kristoffer Severinsen <kristoffer.severinsen@bekk.no>
2021-02-16 13:21:06 +0100

    removed value attribute from select, and filter options for unique values

--- src/Nordea/Components/Dropdown.elm
+++ src/Nordea/Components/Dropdown.elm
@@ -30,6 +30,7 @@ import Css
 import Html.Styled as Html exposing (Attribute, Html, styled)
 import Html.Styled.Attributes as Attributes
 import Html.Styled.Events as Events
+import List.Extra as List
 import Nordea.Resources.Colors as Colors
 import Nordea.Util.List as List
 
@@ -59,7 +60,7 @@ init : String -> List Option -> Dropdown msg
 init value options =
     Dropdown
         { value = value
-        , options = options
+        , options = options |> List.uniqueBy .value
         , onInput = Nothing
         }
 
@@ -93,9 +94,7 @@ viewOption selected option =
 getAttributes : Config msg -> List (Attribute msg)
 getAttributes config =
     List.filterMaybe
-        [ Just config.value |> Maybe.map Attributes.value
-        , config.onInput |> Maybe.map Events.onInput
-        ]
+        [ config.onInput |> Maybe.map Events.onInput ]
 
 
 
