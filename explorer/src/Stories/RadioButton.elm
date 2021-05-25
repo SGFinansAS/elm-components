b3ca65a4c683356a40946314b27900ccabbd6fed
Markus Hauge <markus.hauge@hotmail.com>
2021-05-25 13:35:40 +0200

    Fix radio button styling and improve story (#18)

--- explorer/src/Stories/RadioButton.elm
+++ explorer/src/Stories/RadioButton.elm
@@ -1,6 +1,8 @@
 module Stories.RadioButton exposing (stories)
 
+import Css
 import Html.Styled as Html exposing (text)
+import Html.Styled.Attributes as Attrs
 import Nordea.Components.RadioButton as RadioButton
 import UIExplorer exposing (UI)
 import UIExplorer.Styled exposing (styledStoriesOf)
@@ -11,12 +13,20 @@ stories =
     styledStoriesOf
         "RadioButton"
         [ ( "Default"
-          , \_ -> Html.div [] [
-                RadioButton.init False
-                    |> RadioButton.view [] [ text "Click me" ]
-                    ,
-                RadioButton.init False
-                    |> RadioButton.view [] [ text "Click me" ]
+          , \_ ->
+                Html.div []
+                    [ Html.styled Html.div
+                        [ Css.margin2 (Css.rem 0.5) Css.zero ]
+                        []
+                        [ RadioButton.init False
+                            |> RadioButton.view [ Attrs.name "radio" ] [ text "Answer 1" ]
+                        ]
+                    , Html.styled Html.div
+                        [ Css.margin2 (Css.rem 0.5) Css.zero ]
+                        []
+                        [ RadioButton.init False
+                            |> RadioButton.view [ Attrs.name "radio" ] [ text "Answer 2" ]
+                        ]
                     ]
           , {}
           )
