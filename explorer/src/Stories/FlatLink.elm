2c2aae3e700672495fd674a7a74d7a22e81ef7b4
Hung Quang Thieu <hqthieu@gmail.com>
2021-06-24 13:55:25 +0200

    Support for FlatLink styled as a Button (#22)
    * Added button styles to flatlink
    
    * change some logic
    
    * fix

--- explorer/src/Stories/FlatLink.elm
+++ explorer/src/Stories/FlatLink.elm
@@ -4,6 +4,7 @@ import Css exposing (marginBottom, rem)
 import Css.Global exposing (children, everything)
 import Html.Styled as Html exposing (text)
 import Html.Styled.Attributes exposing (css, href)
+import Nordea.Components.Button as ButtonVariant
 import Nordea.Components.FlatLink as FlatLink
 import Nordea.Resources.Icons as Icons
 import UIExplorer exposing (UI)
@@ -50,6 +51,21 @@ stories =
                         |> FlatLink.view [ href "/#Default/FlatLink/Mini" ] [ text "Click me", Icons.rightIcon Icons.info ]
                     , FlatLink.mini
                         |> FlatLink.view [ href "/#Default/FlatLink/Mini" ] [ Icons.leftIcon Icons.info, text "Click me" ]
+                    ]
+          , {}
+          )
+        , ( "withButtonStyle"
+          , \_ ->
+                Html.div [ css [ children [ everything [ marginBottom (rem 1) ] ] ] ]
+                    [ FlatLink.default
+                        |> FlatLink.withButtonStyle ButtonVariant.Primary
+                        |> FlatLink.view [ href "/#Default/FlatLink/withButtonStyle" ] [ text "Click me" ]
+                    , FlatLink.default
+                        |> FlatLink.withButtonStyle ButtonVariant.Primary
+                        |> FlatLink.view [ href "/#Default/FlatLink/withButtonStyle" ] [ text "Click me", Icons.rightIcon Icons.info ]
+                    , FlatLink.default
+                        |> FlatLink.withButtonStyle ButtonVariant.Primary
+                        |> FlatLink.view [ href "/#Default/FlatLink/withButtonStyle" ] [ Icons.leftIcon Icons.info, text "Click me" ]
                     ]
           , {}
           )
