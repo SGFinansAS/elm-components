451f96e4cae45a3d3f2bfb484e63cb6dfead5c74
Rostislav013 <rostislav013@gmail.com>
2021-11-16 16:11:35 +0200

    Fix styles

--- explorer/src/Stories/FeatureBox.elm
+++ explorer/src/Stories/FeatureBox.elm
@@ -1,7 +1,9 @@
 module Stories.FeatureBox exposing (stories)
 
+import Html.Styled as Html
 import Config exposing (Msg(..))
 import Nordea.Components.FeatureBox as FeatureBox
+import Nordea.Components.Button as Button
 import Nordea.Resources.Icons as Icons
 import UIExplorer exposing (UI)
 import UIExplorer.Styled exposing (styledStoriesOf)
@@ -12,7 +14,12 @@ stories =
         "FeatureBox"
         [ ( "FeatureBox"
           , \_ ->
-                FeatureBox.init True NoOp  "New feature" "Description for new feature"
+                FeatureBox.init True NoOp  "New feature" "Place tolltip message here thats is over 2 lines max"
+                    |> FeatureBox.withIcon Icons.confetti2
+                    |> FeatureBox.withButton 
+                        (Button.primary
+                            |> Button.view [] [ Html.text "Click me" ]
+                        )
                     |> FeatureBox.view 
                     
           , {}
--- src/Nordea/Components/FeatureBox.elm
+++ src/Nordea/Components/FeatureBox.elm
@@ -1,5 +1,48 @@
-module Nordea.Components.FeatureBox exposing (FeatureBox, init, view)
+module Nordea.Components.FeatureBox exposing
+    ( FeatureBox
+    , init
+    , view
+    , withButton
+    , withIcon
+    )
 
+import Css
+    exposing
+        ( Style
+        , alignItems
+        , auto
+        , backgroundColor
+        , bottom
+        , center
+        , column
+        , displayFlex
+        , fixed
+        , flexDirection
+        , height
+        , important
+        , int
+        , justifyContent
+        , left
+        , marginBottom
+        , marginLeft
+        , marginRight
+        , marginTop
+        , minWidth
+        , none
+        , outline
+        , overflow
+        , padding
+        , padding4
+        , pct
+        , position
+        , pseudoClass
+        , rem
+        , textAlign
+        , top
+        , width
+        , zIndex
+        )
+import Css.Global as Css exposing (children, everything)
 import Html.Styled
     exposing
         ( Html
@@ -8,8 +51,14 @@ import Html.Styled
         , p
         , text
         )
-import Nordea.Components.Accordion exposing (Msg)
+import Html.Styled.Attributes exposing (css)
+import Html.Styled.Events exposing (onClick)
+import Nordea.Components.Button as Button
+import Nordea.Components.Text as Text exposing (..)
+import Nordea.Html as Html exposing (showIf)
+import Nordea.Resources.Colors as Colors
 import Nordea.Resources.Icons as Icons
+import Nordea.Themes as Themes
 import Svg.Styled as Svg exposing (Svg)
 
 
@@ -41,11 +90,59 @@ init showModal closeMsg name description =
 
 view : FeatureBox msg -> Html msg
 view (FeatureBox config) =
-    div []
-        [ Icons.confetti
-        , h2 [] [ text config.name ]
-        , p [] [ text config.description ]
-        ]
+    showIf config.showModal
+        (div
+            [ css
+                [ position fixed
+                , left (rem 0)
+                , top (rem 0)
+                , bottom (rem 0)
+                , minWidth (pct 100)
+                , outline none
+                , overflow auto
+                , displayFlex
+                , justifyContent center
+                , alignItems center
+                , padding4 (rem 8) (rem 1) (rem 2) (rem 1)
+                , backgroundColor (Colors.black |> Colors.withAlpha 0.5)
+                , zIndex (int 1)
+                ]
+            ]
+            [ div
+                [ css
+                    [ displayFlex
+                    , alignItems center
+                    , flexDirection column
+                    , backgroundColor Colors.white
+                    , Css.borderRadius (rem 0.5)
+                    , width (rem 20.5)
+                    , children
+                        [ everything
+                            [ pseudoClass "not(:last-child)" [ marginBottom (rem 0.5) ] ]
+                        ]
+                    , padding (rem 1.5)
+                    ]
+                ]
+                [ closeButton config.closeMsg
+                , config.icon
+                    |> Html.viewMaybe
+                        (\icon ->
+                            div [ css [ marginBottom (rem 1.5) |> Css.important ] ] [ icon ]
+                        )
+                , Text.titleHeavy
+                    |> Text.withHtmlTag h2
+                    |> Text.view [] [ text config.name ]
+                , Text.bodyTextSmall
+                    |> Text.withHtmlTag p
+                    |> Text.view [ css [ textAlign center ] ] [ text config.description ]
+                , config.button
+                    |> Html.viewMaybe
+                        (\button ->
+                            div [ css [ marginTop (rem 1), marginBottom (rem 1) ] ] [ button ]
+                        )
+                ]
+            ]
+        )
 
 
 withButton : Html msg -> FeatureBox msg -> FeatureBox msg
@@ -56,3 +153,20 @@ withButton button (FeatureBox config) =
 withIcon : Svg msg -> FeatureBox msg -> FeatureBox msg
 withIcon icon (FeatureBox config) =
     FeatureBox { config | icon = Just icon }
+
+
+closeButton : msg -> Html msg
+closeButton closeMsg =
+    Button.tertiary
+        |> Button.view
+            [ css [ alignItems center, marginLeft auto, marginRight (rem -1), marginTop (rem -0.5) ]
+            , onClick closeMsg
+            ]
+            [ Icons.cross
+                [ css
+                    [ width (rem 1)
+                    , height (rem 1)
+                    , Themes.color Themes.PrimaryColor Colors.blueDeep
+                    ]
+                ]
+            ]
