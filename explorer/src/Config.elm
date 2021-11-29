6d8e7c459c4a4c9263487d5f84a0e8f4d6013748
Mathilde Wærstad <mathildewaerstad@gmail.com>
2021-11-29 14:49:49 +0100

    Add the possibility to close the feature box so its possible to click on other menu items (#79)

--- explorer/src/Config.elm
+++ explorer/src/Config.elm
@@ -5,13 +5,18 @@ import Nordea.Components.Accordion as Accordion exposing (Accordion)
 
 
 type alias Config =
-    { accordion : Accordion, isModalOpen : Bool, isProgressBarCompleted : Bool }
+    { accordion : Accordion
+    , isModalOpen : Bool
+    , isFeatureBoxOpen : Bool
+    , isProgressBarCompleted : Bool
+    }
 
 
 type Msg
     = AccordionMsg Accordion.Msg
     | NoOp
     | ToggleModal
+    | ToggleFeatureBox
     | ToggleProgressBarCompleted
 
 
@@ -31,6 +36,7 @@ init =
                 , open = False
                 }
     , isModalOpen = True
+    , isFeatureBoxOpen = True
     , isProgressBarCompleted = False
     }
 
@@ -46,6 +52,9 @@ update msg config =
 
         ToggleModal ->
             { config | isModalOpen = not config.isModalOpen }
+
+        ToggleFeatureBox ->
+            { config | isFeatureBoxOpen = not config.isFeatureBoxOpen }
 
         ToggleProgressBarCompleted ->
             { config | isProgressBarCompleted = not config.isProgressBarCompleted }
