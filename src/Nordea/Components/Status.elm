5deedfcffd11ffdd848e53e2ced1d3209d930bad
Fredrik Løberg <fredrik.loberg@bekk.no>
2021-12-14 14:59:48 +0100

    Update status component typography

--- src/Nordea/Components/Status.elm
+++ src/Nordea/Components/Status.elm
@@ -1,8 +1,25 @@
 module Nordea.Components.Status exposing (blue, gray, green, red, yellow)
 
-import Css exposing (Color, backgroundColor, borderRadius, display, ellipsis, fontSize, hidden, inlineBlock, lineHeight, maxWidth, overflow, padding, pct, rem, textOverflow)
-import Html.Styled as Html exposing (Html)
+import Css
+    exposing
+        ( Color
+        , backgroundColor
+        , borderRadius
+        , display
+        , ellipsis
+        , fontSize
+        , hidden
+        , inlineBlock
+        , lineHeight
+        , overflow
+        , padding
+        , pct
+        , rem
+        , textOverflow
+        )
+import Html.Styled as Html exposing (Attribute, Html)
 import Html.Styled.Attributes exposing (css)
+import Nordea.Components.Text as Text
 import Nordea.Resources.Colors as Color
 
 
@@ -14,64 +31,61 @@ type StatusColor
     | LightGray
 
 
-view : String -> StatusColor -> Html msg
-view text statusColor =
-    Html.span
-        [ css
-            [ padding (rem 0.5)
-            , backgroundColor (statusToColor statusColor)
-            , borderRadius (rem 0.75)
-            , fontSize (rem 0.75)
-            , display inlineBlock
-            , lineHeight (rem 1)
-            , maxWidth (pct 100)
-            , textOverflow ellipsis
-            , overflow hidden
-            ]
-        ]
-        [ Html.text text
-        ]
+view : String -> StatusColor -> List (Attribute msg) -> Html msg
+view text statusColor attrs =
+    let
+        backgroundColorForStatus =
+            case statusColor of
+                Green ->
+                    Color.greenStatus
+
+                Red ->
+                    Color.redStatus
+
+                CloudBlue ->
+                    Color.blueCloudStatus
+
+                Yellow ->
+                    Color.yellowStatus
+
+                LightGray ->
+                    Color.grayLightStatus
+    in
+    Text.textTinyLight
+        |> Text.view
+            (css
+                [ display inlineBlock
+                , borderRadius (rem 0.75)
+                , padding (rem 0.5)
+                , backgroundColor backgroundColorForStatus
+                , textOverflow ellipsis
+                , overflow hidden
+                ]
+                :: attrs
+            )
+            [ Html.text text ]
 
 
-green : String -> Html msg
+green : String -> List (Attribute msg) -> Html msg
 green text =
     view text Green
 
 
-yellow : String -> Html msg
+yellow : String -> List (Attribute msg) -> Html msg
 yellow text =
     view text Yellow
 
 
-red : String -> Html msg
+red : String -> List (Attribute msg) -> Html msg
 red text =
     view text Red
 
 
-blue : String -> Html msg
+blue : String -> List (Attribute msg) -> Html msg
 blue text =
     view text CloudBlue
 
 
-gray : String -> Html msg
+gray : String -> List (Attribute msg) -> Html msg
 gray text =
     view text LightGray
-
-
-statusToColor : StatusColor -> Color
-statusToColor statusColor =
-    case statusColor of
-        Green ->
-            Color.greenStatus
-
-        Red ->
-            Color.redStatus
-
-        CloudBlue ->
-            Color.blueCloudStatus
-
-        Yellow ->
-            Color.yellowStatus
-
-        LightGray ->
-            Color.grayLightStatus
