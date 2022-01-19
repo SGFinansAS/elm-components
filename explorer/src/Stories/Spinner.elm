4d0a2197084786b4fd3e97a1a07f1322e7d5ddef
Hung Quang Thieu <hqthieu@gmail.com>
2022-01-19 09:04:57 +0100

    Add spinner with custom width

--- explorer/src/Stories/Spinner.elm
+++ explorer/src/Stories/Spinner.elm
@@ -1,6 +1,8 @@
 module Stories.Spinner exposing (..)
 
 import Config exposing (Config, Msg(..))
+import Css exposing (rem, width)
+import Html.Styled.Attributes exposing (css)
 import Nordea.Components.Spinner as Spinner
 import UIExplorer exposing (UI)
 import UIExplorer.Styled exposing (styledStoriesOf)
@@ -12,6 +14,10 @@ stories =
         "Spinner"
         [ ( "Small"
           , \_ -> Spinner.small []
+          , {}
+          )
+        , ( "Custom"
+          , \_ -> Spinner.custom [ css [ width (rem 1) ] ]
           , {}
           )
         ]
