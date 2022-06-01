b7d6ba217666f4a314d6bddeedfa618eac5a6f67
Vetle Solgaard <vetle.busol@outlook.com>
2022-06-01 08:39:30 +0200

    Hackathon-174: Separated illustrsations into own file

--- explorer/src/Stories/Icons.elm
+++ explorer/src/Stories/Icons.elm
@@ -1,10 +1,9 @@
 module Stories.Icons exposing (stories)
 
 import Css exposing (column, displayFlex, flexDirection, rem, row, width)
-import Html.Styled as Html
+import Html.Styled as Html exposing (Html)
 import Html.Styled.Attributes exposing (css)
 import Nordea.Components.Text as Text
-import Nordea.Resources.Colors as Colors
 import Nordea.Resources.Icons as Icons
 import UIExplorer exposing (UI)
 import UIExplorer.Styled exposing (styledStoriesOf)
