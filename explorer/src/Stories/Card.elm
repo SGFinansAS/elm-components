a052d52b2b8842c3e93a3676f6236246d60184c2
Fredrik Løberg <fredrik.loberg@bekk.no>
2022-08-03 14:15:01 +0200

    fix

--- explorer/src/Stories/Card.elm
+++ explorer/src/Stories/Card.elm
@@ -35,6 +35,30 @@ stories =
                     |> Card.view [] [ Html.text "Card content" ]
           , {}
           )
+        , ( "With custom header content"
+          , \_ ->
+                Card.init
+                    |> Card.withShadow
+                    |> Card.view []
+                        [ Card.header []
+                            [ Card.title [] [ Html.text "Card title" ]
+                            , Html.text "Custom header content"
+                            ]
+                        , Html.text "Card content"
+                        ]
+          , {}
+          )
+        , ( "With footer"
+          , \_ ->
+                Card.init
+                    |> Card.withTitle "Card title"
+                    |> Card.withShadow
+                    |> Card.view []
+                        [ Html.text "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        , Card.footer [] [ Html.text "Some footer content" ]
+                        ]
+          , {}
+          )
         , ( "With title and infobox"
           , \_ ->
                 Card.init
