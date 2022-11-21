71109d93baac838d0037a18cb9b58bed01362da3
Solgård <vetle.busol@outlook.com>
2022-11-21 13:06:13 +0100

    PAR-2020: Added focus and hover effects

--- src/Nordea/Components/FilterChip.elm
+++ src/Nordea/Components/FilterChip.elm
@@ -3,17 +3,25 @@ module Nordea.Components.FilterChip exposing (init, view)
 import Css
     exposing
         ( alignItems
+        , backgroundColor
         , borderBox
         , borderRadius
         , borderWidth
         , boxSizing
         , center
+        , cursor
         , displayFlex
         , fitContent
         , flexDirection
+        , focus
+        , hover
         , maxWidth
+        , minHeight
         , noWrap
+        , none
+        , outline
         , padding2
+        , pointer
         , rem
         , whiteSpace
         , width
@@ -21,7 +29,7 @@ import Css
 import Html.Styled as Html exposing (Attribute, Html)
 import Html.Styled.Attributes exposing (css)
 import Nordea.Components.Text as Text
-import Nordea.Resources.Colors as Colors
+import Nordea.Resources.Colors as Color
 import Nordea.Resources.Icons as Icon
 import Nordea.Themes as Themes
 
@@ -40,18 +48,26 @@ view attrs (FilterChip { label }) =
     Html.button
         ([ css
             [ padding2 (rem 0.25) (rem 0.75)
+            , minHeight fitContent
             , displayFlex
             , flexDirection Css.row
             , borderRadius (rem 2)
             , boxSizing borderBox
-            , Themes.backgroundColor Themes.SecondaryColor Colors.cloudBlue
-            , Themes.color Themes.PrimaryColor Colors.deepBlue
+            , Themes.backgroundColor Themes.SecondaryColor Color.cloudBlue
+            , Themes.color Themes.PrimaryColor Color.deepBlue
             , maxWidth fitContent
             , Css.property "gap" "1rem"
+            , Css.property "height" "fit-content"
             , whiteSpace noWrap
             , alignItems center
             , borderWidth (rem 0)
             , Css.property "appearance" "none"
+            , cursor pointer
+            , hover [ backgroundColor Color.lightGray ]
+            , focus
+                [ outline none
+                , Css.property "box-shadow" ("0rem 0rem 0rem 0.125rem " ++ Themes.colorVariable Themes.SecondaryColor Color.blueHaas)
+                ]
             ]
          ]
             ++ attrs
