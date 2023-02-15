27de0bfff1f56c7877d691bad7cde4e13651f097
Murat Topak <murattopak01@gmail.com>
2023-02-15 23:25:14 +0100

    remove padding

--- explorer/src/Stories/AccordionMenu.elm
+++ explorer/src/Stories/AccordionMenu.elm
@@ -1,7 +1,7 @@
 module Stories.AccordionMenu exposing (..)
 
 import Config exposing (Config, Msg(..))
-import Css exposing (cursor, fontSize, listStyle, marginRight, marginTop, middle, none, paddingLeft, pointer, rem, verticalAlign, width)
+import Css exposing (cursor, fontSize, listStyle, marginRight, marginTop, middle, none, padding, paddingLeft, pointer, rem, verticalAlign, width)
 import Css.Global exposing (children, typeSelector)
 import Html.Styled exposing (div, li, span, text, ul)
 import Html.Styled.Attributes exposing (css)
@@ -18,7 +18,7 @@ stories =
         [ ( "Default"
           , \_ ->
                 AccordionMenu.view
-                    {isOpen = True}
+                    { isOpen = True }
                     []
                     []
                     [ AccordionMenu.header [] [ text "Kontrakt e-signert 10.10.22" ]
@@ -35,14 +35,14 @@ stories =
         , ( "Nested"
           , \_ ->
                 AccordionMenu.view
-                    {isOpen = True}
+                    { isOpen = True }
                     []
                     []
                     [ AccordionMenu.header [] [ text "Parent Accordion " ]
                     , AccordionMenu.content [ css [ paddingLeft (rem 2) ] ]
                         [ text "Description"
                         , AccordionMenu.view
-                            {isOpen = True}
+                            { isOpen = True }
                             []
                             []
                             [ AccordionMenu.header [] [ text "Child Accordion" ]
@@ -61,12 +61,12 @@ stories =
         , ( "Styled"
           , \_ ->
                 AccordionMenu.view
-                    {isOpen = True}
+                    { isOpen = True }
                     []
                     []
                     [ AccordionMenu.header [ css [ cursor pointer ] ] [ Icons.filledCheckmark [ css [ verticalAlign middle, width (rem 2), marginRight (rem 0.5) ] ], text "Kontrakt e-signert 10.10.22" ]
                     , AccordionMenu.content [ css [ fontSize (rem 0.9) ] ]
-                        [ ul [ css [ listStyle none, children [ typeSelector "li" [ marginTop (rem 0.5) ] ] ] ]
+                        [ ul [ css [ padding (rem 0), listStyle none, children [ typeSelector "li" [ marginTop (rem 0.5) ] ] ] ]
                             [ li [] [ Icons.filledInfo [ css [ verticalAlign middle, width (rem 2), marginRight (rem 0.5) ] ], text "Undertegner identifisert 09.10.22" ]
                             , li [] [ Icons.dismiss [ css [ verticalAlign middle, width (rem 2), marginRight (rem 0.5) ] ], text "E-signering utloper snart 08.10.22" ]
                             , li [] [ Icons.filledWarning [ css [ verticalAlign middle, width (rem 2), marginRight (rem 0.5) ] ], text "E-signering sendt 07.10.22" ]
