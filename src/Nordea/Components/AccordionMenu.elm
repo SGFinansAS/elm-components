fe8e0eb076eb170edb47820e263d8e007f02a843
Murat Topak <murattopak01@gmail.com>
2023-02-16 14:15:22 +0100

    remove wrapper div, float

--- src/Nordea/Components/AccordionMenu.elm
+++ src/Nordea/Components/AccordionMenu.elm
@@ -1,9 +1,9 @@
 module Nordea.Components.AccordionMenu exposing (content, header, view)
 
-import Css exposing (alignItems, center, display, displayFlex, float, inlineBlock, justifyContent, listStyle, none, rem, right, spaceBetween, width)
+import Css exposing (alignItems, center, display, displayFlex, inlineBlock, justifyContent, listStyle, none, rem, spaceBetween, width)
 import Css.Global exposing (children, class, descendants, typeSelector, withAttribute)
 import Html.Styled as Html exposing (Attribute, Html, div)
-import Html.Styled.Attributes exposing (attribute, css)
+import Html.Styled.Attributes as Html exposing (attribute, css)
 import Nordea.Resources.Icons as Icons
 
 
@@ -23,8 +23,8 @@ view config _ attrs children_ =
 
         attrs_ =
             css
-                [ children [ typeSelector "summary" [ listStyle none, descendants [ class "accordion-close-icon" [ display inlineBlock ], class "accordion-open-icon" [ display none ] ] ] ]
-                , withAttribute "open" [ children [ typeSelector "summary" [ descendants [ class "accordion-close-icon" [ display none ], class "accordion-open-icon" [ display inlineBlock ] ] ] ] ]
+                [ children [ typeSelector "summary" [ listStyle none, descendants [ class "accordion-closed-icon" [ display inlineBlock ], class "accordion-open-icon" [ display none ] ] ] ]
+                , withAttribute "open" [ children [ typeSelector "summary" [ descendants [ class "accordion-closed-icon" [ display none ], class "accordion-open-icon" [ display inlineBlock ] ] ] ] ]
                 ]
                 :: openAttr
     in
@@ -36,8 +36,8 @@ header attrs children =
     Html.summary
         (css [ displayFlex, alignItems center, justifyContent spaceBetween ] :: attrs)
         [ div [] children
-        , div [ Html.Styled.Attributes.class "accordion-open-icon", css [ float right ] ] [ Icons.chevronDown [ css [ width (rem 1.5) ] ] ]
-        , div [ Html.Styled.Attributes.class "accordion-close-icon", css [ float right ] ] [ Icons.chevronUp [ css [ width (rem 1.5) ] ] ]
+        , Icons.chevronDown [ Html.class "accordion-open-icon", css [ width (rem 1.5) ] ]
+        , Icons.chevronUp [ Html.class "accordion-closed-icon", css [ width (rem 1.5) ] ]
         ]
 
 
