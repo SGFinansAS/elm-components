3c19c2c129eac87bde653b68f3cbca0fef7b24af
Heidi Tarkiainen <109804862+heiditark@users.noreply.github.com>
2023-10-10 13:17:03 +0300

    PAR-2347: Collapsible InformationDetails Card (#321)
    Implemented collapsible Card in addition to the regular one. Collapsible
    card component is used in InformationDetails. 'emphasisedText' fades in
    and out when card is collapsed/uncollapsed.
    
    
    ![image](https://github.com/SGFinansAS/elm-components/assets/109804862/1c566d2a-6a25-41c0-8eca-40d6d3ac6169)
    
    ![image](https://github.com/SGFinansAS/elm-components/assets/109804862/258a7513-0715-4747-bc46-f95b8632fdbc)

--- explorer/src/Stories/InformationDetails.elm
+++ explorer/src/Stories/InformationDetails.elm
@@ -3,8 +3,9 @@ module Stories.InformationDetails exposing (stories)
 import Config exposing (Config, Msg(..))
 import Css
 import Html.Styled as Html
-import Html.Styled.Attributes exposing (css)
+import Html.Styled.Attributes as Html exposing (css)
 import Nordea.Components.InformationDetails as InformationDetails
+import Nordea.Components.Text as Text
 import Nordea.Resources.Colors as Colors
 import UIExplorer exposing (UI)
 import UIExplorer.Styled exposing (styledStoriesOf)
@@ -22,7 +23,8 @@ stories =
                         , Css.padding (Css.rem 2)
                         ]
                     ]
-                    [ InformationDetails.card []
+                    [ InformationDetails.card
+                        []
                         [ InformationDetails.fullWidthElement []
                             [ InformationDetails.label [] [ Html.text "Full width" ]
                             , InformationDetails.value [] [ Html.text "Full width" ]
@@ -49,6 +51,61 @@ stories =
                             ]
                         ]
                         (Just "Title")
+                        Nothing
+                    ]
+          , {}
+          )
+        , ( "Collapsible"
+          , \_ ->
+                Html.div
+                    [ css
+                        [ Css.backgroundColor Colors.lightGray
+                        , Css.padding (Css.rem 2)
+                        ]
+                    ]
+                    [ InformationDetails.card
+                        []
+                        [ InformationDetails.fullWidthElement []
+                            [ InformationDetails.label [] [ Html.text "Full width" ]
+                            , InformationDetails.value [] [ Html.text "Full width" ]
+                            ]
+                        , InformationDetails.element []
+                            [ InformationDetails.label [] [ Html.text "Noe" ]
+                            , InformationDetails.value [] [ Html.text "Noe" ]
+                            ]
+                        , InformationDetails.element []
+                            [ InformationDetails.label [] [ Html.text "Noe mer" ]
+                            , InformationDetails.value [] [ Html.text "Noe mer" ]
+                            ]
+                        , InformationDetails.element []
+                            [ InformationDetails.label [] [ Html.text "Enda mer" ]
+                            , InformationDetails.value [] [ Html.text "Enda mer" ]
+                            ]
+                        , InformationDetails.fullWidthElement []
+                            [ InformationDetails.label []
+                                [ Html.text "Label"
+                                ]
+                            , InformationDetails.value [] [ Html.text "1" ]
+                            , InformationDetails.value [] [ Html.text "2" ]
+                            , InformationDetails.value [] [ Html.text "3" ]
+                            ]
+                        ]
+                        (Just "Title")
+                        (Just
+                            { emphasisedText =
+                                Text.textLight
+                                    |> Text.view
+                                        [ css
+                                            [ Css.paddingRight (Css.rem 0.75)
+                                            , Css.maxWidth (Css.rem 13.25)
+                                            , Css.textOverflow Css.ellipsis
+                                            , Css.overflow Css.hidden
+                                            ]
+                                        ]
+                                        [ Html.text "Emphasised Text" ]
+                            , isOpen = False
+                            }
+                        )
                     ]
           , {}
           )
