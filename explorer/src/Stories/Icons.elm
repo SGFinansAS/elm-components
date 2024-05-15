8c2b7060e3282f02be08da6fb92af34c41fcc865
alex-singh <alex_ad_singh@hotmail.com>
2024-05-15 14:11:53 +0300

    add pdf icon to explorer

--- explorer/src/Stories/Icons.elm
+++ explorer/src/Stories/Icons.elm
@@ -66,6 +66,7 @@ stories =
                         , Icons.calendar [ css [ width (rem 1.5) ] ]
                         , Icons.visible [ css [ width (rem 1.5) ] ]
                         , Icons.invisible [ css [ width (rem 1.5) ] ]
+                        , Icons.pdf [ css [ width (rem 1.5) ] ]
                         ]
                     , iconCategoryHeader "Add, remove, accept and cancel"
                     , iconsRow
