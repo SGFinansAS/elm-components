34c40f016096c502897fc29aa5b6161e15bd4942
Fredrik Løberg <42603101+fredrikloberg@users.noreply.github.com>
2024-06-24 15:14:07 +0200

    Small tweaks (#388)
    * Add utility for css with zero precedence. (Very useful for components
    where you want to set a default that can be overridden without using
    Css.important)
    * Add support for clicking esc to dismiss dropdown
    * Minor minifying to js code

--- src/Nordea/Components/OnClickOutsideSupport.elm
+++ src/Nordea/Components/OnClickOutsideSupport.elm
@@ -16,24 +16,32 @@ view { isActive } =
              else
                 "false"
             )
-        , srcdoc
-            """
-                <script>
-                    const doc = window.parent.document;
-                    const id = "click-outside-" + Math.random();
-                    const frameEl = window.frameElement;
-                    frameEl.setAttribute("id", id);
-
-                    const listener = event => {
-                        if (!doc.getElementById(id)) {
-                            doc.removeEventListener("click", listener);
-                        } else if (frameEl.dataset.isActive === "true" && !frameEl.parentElement.contains(event.target)) {
-                            frameEl.parentElement.dispatchEvent(new CustomEvent('outsideclick'));
-                        }
-                    };
-
-                    doc.addEventListener("click", listener);
-                </script>
-            """
+        , srcdoc jsCode
         ]
         []
+
+
+jsCode =
+    """
+    <script>
+        const doc = window.parent.document;
+        const f = window.frameElement;
+
+        const id = "co-" + Math.random();
+        f.setAttribute("id", id);
+
+        const listener = e => {
+            if (!doc.getElementById(id)) {
+                doc.removeEventListener("click", listener);
+            } else if (f.dataset.isActive === "true" && !f.parentElement.contains(e.target)) {
+                f.parentElement.dispatchEvent(new CustomEvent('outsideclick'));
+            }
+        };
+
+        doc.addEventListener("click", listener);
+    </script>
+    """
+        |> String.split "\n"
+        |> List.map String.trim
+        |> List.filter (String.isEmpty >> not)
+        |> String.join "\n"
