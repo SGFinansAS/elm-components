159a92129c2ce862613faaa740c83281f5c01764
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2024-09-17 12:50:13 +0200

    Rename var in example

--- explorer/src/Stories/AccordionTable.elm
+++ explorer/src/Stories/AccordionTable.elm
@@ -4,8 +4,8 @@ import Config exposing (Config, Msg)
 import Css exposing (marginBottom, marginLeft, maxWidth, px, rem)
 import Html.Styled exposing (div, text)
 import Html.Styled.Attributes exposing (css)
-import Nordea.Components.AccordionTableHeader as AccordionTableHeader
-import Nordea.Components.AccordionTableRow as AccordionTableRow
+import Nordea.Components.AccordionTableHeader as Header
+import Nordea.Components.AccordionTableRow as Row
 import Nordea.Components.Text as Text
 import Nordea.Css exposing (displayGrid, gridColumn, gridTemplateColumns)
 import Set
@@ -20,63 +20,63 @@ stories =
         [ ( "Default"
           , \config ->
                 div [ css [ displayGrid, gridTemplateColumns "1fr auto", maxWidth (px 800) ] ]
-                    [ AccordionTableHeader.init
-                        |> AccordionTableHeader.view
+                    [ Header.init
+                        |> Header.view
                             [ css [ gridColumn "1/-1" ] ]
                             [ Text.bodyTextLight |> Text.view [] [ text "Table header" ] ]
-                    , AccordionTableRow.init (Set.member 0 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 0)
-                        |> AccordionTableRow.withSummary [] [ Text.textLight |> Text.view [] [ text "Summary" ] ]
-                        |> AccordionTableRow.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details" ] ]
-                        |> AccordionTableRow.view []
-                    , AccordionTableRow.init (Set.member 1 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 1)
-                        |> AccordionTableRow.withSummary [] [ Text.textLight |> Text.view [] [ text "Summary 2" ] ]
-                        |> AccordionTableRow.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details 2" ] ]
-                        |> AccordionTableRow.view []
+                    , Row.init (Set.member 0 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 0)
+                        |> Row.withSummary [] [ Text.textLight |> Text.view [] [ text "Summary" ] ]
+                        |> Row.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details" ] ]
+                        |> Row.view []
+                    , Row.init (Set.member 1 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 1)
+                        |> Row.withSummary [] [ Text.textLight |> Text.view [] [ text "Summary 2" ] ]
+                        |> Row.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details 2" ] ]
+                        |> Row.view []
                     ]
           , {}
           )
         , ( "Multiple columns"
           , \config ->
                 div [ css [ displayGrid, gridTemplateColumns "repeat(3, 1fr) auto", maxWidth (px 800) ] ]
-                    [ AccordionTableHeader.init
-                        |> AccordionTableHeader.view
+                    [ Header.init
+                        |> Header.view
                             [ css [ marginBottom (rem 1) ] ]
                             [ Text.bodyTextLight |> Text.view [] [ text "Table header" ] ]
                     , Text.textLight |> Text.view [ css [ marginLeft (rem 1) ] ] [ text "Name" ]
                     , Text.textLight |> Text.view [] [ text "Occupation" ]
                     , Text.textLight |> Text.view [] [ text "Age" ]
-                    , AccordionTableRow.init (Set.member 0 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 0)
-                        |> AccordionTableRow.withSummary []
+                    , Row.init (Set.member 0 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 0)
+                        |> Row.withSummary []
                             [ Text.textLight |> Text.view [] [ text "Johnny" ]
                             , Text.textLight |> Text.view [] [ text "Carpenter" ]
                             , Text.textLight |> Text.view [] [ text "34" ]
                             ]
-                        |> AccordionTableRow.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details" ] ]
-                        |> AccordionTableRow.view []
-                    , AccordionTableRow.init (Set.member 1 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 1)
-                        |> AccordionTableRow.withSummary []
+                        |> Row.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details" ] ]
+                        |> Row.view []
+                    , Row.init (Set.member 1 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 1)
+                        |> Row.withSummary []
                             [ Text.textLight |> Text.view [] [ text "Maximilian" ]
                             , Text.textLight |> Text.view [] [ text "Musician" ]
                             , Text.textLight |> Text.view [] [ text "23" ]
                             ]
-                        |> AccordionTableRow.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details 2" ] ]
-                        |> AccordionTableRow.view []
+                        |> Row.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details 2" ] ]
+                        |> Row.view []
                     ]
           , {}
           )
         , ( "Small"
           , \config ->
                 div [ css [ displayGrid, gridTemplateColumns "1fr auto", maxWidth (px 600) ] ]
-                    [ AccordionTableHeader.init
-                        |> AccordionTableHeader.withSmallSize
-                        |> AccordionTableHeader.view
+                    [ Header.init
+                        |> Header.withSmallSize
+                        |> Header.view
                             [ css [ gridColumn "1/-1" ] ]
                             [ Text.bodyTextSmall |> Text.view [] [ text "Table header" ] ]
-                    , AccordionTableRow.init (Set.member 0 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 0)
-                        |> AccordionTableRow.withSmallSize
-                        |> AccordionTableRow.withSummary [] [ Text.textSmallLight |> Text.view [] [ text "Summary" ] ]
-                        |> AccordionTableRow.withDetails [] [ Text.bodyTextSmall |> Text.view [] [ text "Details" ] ]
-                        |> AccordionTableRow.view []
+                    , Row.init (Set.member 0 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 0)
+                        |> Row.withSmallSize
+                        |> Row.withSummary [] [ Text.textSmallLight |> Text.view [] [ text "Summary" ] ]
+                        |> Row.withDetails [] [ Text.bodyTextSmall |> Text.view [] [ text "Details" ] ]
+                        |> Row.view []
                     ]
           , {}
           )
