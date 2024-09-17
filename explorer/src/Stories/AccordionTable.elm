8a92ab9f3318dadaf0774650491aa78f065d6a1f
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2024-09-17 13:06:44 +0200

    Better examples

--- explorer/src/Stories/AccordionTable.elm
+++ explorer/src/Stories/AccordionTable.elm
@@ -1,13 +1,14 @@
 module Stories.AccordionTable exposing (stories)
 
 import Config exposing (Config, Msg)
-import Css exposing (marginBottom, marginLeft, maxWidth, px, rem)
+import Css exposing (color, marginBottom, marginLeft, marginTop, maxWidth, px, rem)
 import Html.Styled exposing (div, text)
 import Html.Styled.Attributes exposing (css)
 import Nordea.Components.AccordionTableHeader as Header
 import Nordea.Components.AccordionTableRow as Row
 import Nordea.Components.Text as Text
 import Nordea.Css exposing (displayGrid, gridColumn, gridTemplateColumns)
+import Nordea.Resources.Colors as Colors
 import Set
 import UIExplorer exposing (UI)
 import UIExplorer.Styled exposing (styledStoriesOf)
@@ -23,14 +24,14 @@ stories =
                     [ Header.init
                         |> Header.view
                             [ css [ gridColumn "1/-1" ] ]
-                            [ Text.bodyTextLight |> Text.view [] [ text "Table header" ] ]
+                            [ Text.textLight |> Text.view [] [ text "Table header" ] ]
                     , Row.init (Set.member 0 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 0)
-                        |> Row.withSummary [] [ Text.textLight |> Text.view [] [ text "Summary" ] ]
-                        |> Row.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details" ] ]
+                        |> Row.withSummary [] [ Text.textLight |> Text.view [] [ text "Some summary" ] ]
+                        |> Row.withDetails [] [ Text.textLight |> Text.view [] [ text "Details" ] ]
                         |> Row.view []
                     , Row.init (Set.member 1 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 1)
-                        |> Row.withSummary [] [ Text.textLight |> Text.view [] [ text "Summary 2" ] ]
-                        |> Row.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details 2" ] ]
+                        |> Row.withSummary [] [ Text.textLight |> Text.view [] [ text "Some other summary" ] ]
+                        |> Row.withDetails [] [ Text.textLight |> Text.view [] [ text "More details" ] ]
                         |> Row.view []
                     ]
           , {}
@@ -40,18 +41,20 @@ stories =
                 div [ css [ displayGrid, gridTemplateColumns "repeat(3, 1fr) auto", maxWidth (px 800) ] ]
                     [ Header.init
                         |> Header.view
-                            [ css [ marginBottom (rem 1) ] ]
-                            [ Text.bodyTextLight |> Text.view [] [ text "Table header" ] ]
-                    , Text.textLight |> Text.view [ css [ marginLeft (rem 1) ] ] [ text "Name" ]
-                    , Text.textLight |> Text.view [] [ text "Occupation" ]
-                    , Text.textLight |> Text.view [] [ text "Age" ]
+                            []
+                            [ Text.textLight |> Text.view [] [ text "People" ] ]
+                    , div [ css [ color Colors.nordeaGray, marginTop (rem 1), displayGrid, gridTemplateColumns "subgrid", gridColumn "1/-1" ] ]
+                        [ Text.textLight |> Text.view [ css [ marginLeft (rem 1) ] ] [ text "Name" ]
+                        , Text.textLight |> Text.view [] [ text "Occupation" ]
+                        , Text.textLight |> Text.view [] [ text "Age" ]
+                        ]
                     , Row.init (Set.member 0 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 0)
                         |> Row.withSummary []
                             [ Text.textLight |> Text.view [] [ text "Johnny" ]
                             , Text.textLight |> Text.view [] [ text "Carpenter" ]
                             , Text.textLight |> Text.view [] [ text "34" ]
                             ]
-                        |> Row.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details" ] ]
+                        |> Row.withDetails [] [ Text.textLight |> Text.view [] [ text "Details about Johnny" ] ]
                         |> Row.view []
                     , Row.init (Set.member 1 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 1)
                         |> Row.withSummary []
@@ -59,7 +62,7 @@ stories =
                             , Text.textLight |> Text.view [] [ text "Musician" ]
                             , Text.textLight |> Text.view [] [ text "23" ]
                             ]
-                        |> Row.withDetails [] [ Text.bodyTextLight |> Text.view [] [ text "Details 2" ] ]
+                        |> Row.withDetails [] [ Text.textLight |> Text.view [] [ text "Details about Maximilian" ] ]
                         |> Row.view []
                     ]
           , {}
@@ -71,11 +74,11 @@ stories =
                         |> Header.withSmallSize
                         |> Header.view
                             [ css [ gridColumn "1/-1" ] ]
-                            [ Text.bodyTextSmall |> Text.view [] [ text "Table header" ] ]
+                            [ Text.textSmallLight |> Text.view [] [ text "Table header" ] ]
                     , Row.init (Set.member 0 config.customModel.openAccordionTableRows) (Config.AccordionTableMsg 0)
                         |> Row.withSmallSize
                         |> Row.withSummary [] [ Text.textSmallLight |> Text.view [] [ text "Summary" ] ]
-                        |> Row.withDetails [] [ Text.bodyTextSmall |> Text.view [] [ text "Details" ] ]
+                        |> Row.withDetails [] [ Text.textSmallLight |> Text.view [] [ text "Details" ] ]
                         |> Row.view []
                     ]
           , {}
