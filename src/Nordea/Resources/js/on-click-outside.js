b334028032135b9005c6cbc5c6b1a11e515ea151
Fredrik Løberg <fredrik.loberg@bekk.no>
2025-04-08 16:12:17 +0200

    fix

--- src/Nordea/Resources/js/on-click-outside.js
+++ src/Nordea/Resources/js/on-click-outside.js
@@ -1,12 +1,9 @@
-const listener = (e) => {
-  document.querySelectorAll(".outside-click").forEach((el) => {
-    if (
-      el.dataset.isActive === "true" &&
-      !el.parentElement.contains(e.target)
-    ) {
-      el.parentElement.dispatchEvent(new CustomEvent("outsideclick"));
+const listener = e => {
+  document.querySelectorAll('.outside-click').forEach(el => {
+    if (el.dataset.isActive === 'true' && !el.parentElement.contains(e.target)) {
+      el.parentElement.dispatchEvent(new CustomEvent('outsideclick'));
     }
   });
 };
 
-document.addEventListener("click", listener);
+document.addEventListener('click', listener);
