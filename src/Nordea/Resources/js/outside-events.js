a27bc10782f274807ad0e745ab6e1b68b6ebe7b4
Morten Kolstad <morten.kolstad@bekk.no>
2025-04-25 14:46:23 +0200

    :

--- src/Nordea/Resources/js/outside-events.js
+++ src/Nordea/Resources/js/outside-events.js
@@ -1,18 +1,10 @@
-const outsideClickListener = e => {
-  document.querySelectorAll('.outside-click').forEach(el => {
+const addListener = name => e => {
+  document.querySelectorAll('.' + name).forEach(el => {
     if (!el.parentElement.contains(e.target)) {
-      el.dispatchEvent(new CustomEvent('outside-click'));
+      el.dispatchEvent(new CustomEvent(name));
     }
   });
 };
 
-const outsideFocusListener = e => {
-  document.querySelectorAll('.outside-focus').forEach(el => {
-    if (!el.parentElement.contains(e.target)) {
-      el.dispatchEvent(new CustomEvent('outside-focus'));
-    }
-  });
-};
-
-document.addEventListener('click', outsideClickListener);
-document.addEventListener('focusin', outsideFocusListener);
+document.addEventListener('click', addListener('outside-click'));
+document.addEventListener('focusin', addListener('outside-focus'));
