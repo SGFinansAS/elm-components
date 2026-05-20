2a0731fb65274827710611d9f75f0ced5a54a268
Heidi Tarkiainen <109804862+heiditark@users.noreply.github.com>
2026-05-20 17:33:56 +0300

    InfoPanel accessibility (#541)

--- src/Nordea/Components/InfoPanel.elm
+++ src/Nordea/Components/InfoPanel.elm
@@ -3,6 +3,8 @@ module Nordea.Components.InfoPanel exposing (InfoPanelContent, view, viewLabelIn
 import Css exposing (int, pseudoClass, rem)
 import Html.Styled as Html exposing (Html)
 import Html.Styled.Attributes as Attributes
+import Nordea.Components.Text as Text
+import Nordea.Html exposing (attrIf)
 import Nordea.Resources.Colors as Colors
 
 
@@ -14,7 +16,7 @@ type alias InfoPanelContent =
 
 view : List InfoPanelContent -> Html msg
 view panelInfoList =
-    Html.div
+    Html.dl
         [ Attributes.css
             [ Css.backgroundColor (Colors.coolGray |> Colors.withAlpha 0.5)
             , Css.boxSizing Css.borderBox
@@ -22,26 +24,20 @@ view panelInfoList =
             , Css.border3 (rem 0.0625) Css.solid Colors.mediumGray
             ]
         ]
-        (panelInfoList |> List.map (\panelInfo -> viewLabelInfoBlock panelInfo))
+        (panelInfoList |> List.indexedMap (\i panelInfo -> viewLabelInfoBlock panelInfo i))
 
 
-viewLabelInfoBlock : InfoPanelContent -> Html msg
-viewLabelInfoBlock infoPanelContent =
+viewLabelInfoBlock : InfoPanelContent -> Int -> Html msg
+viewLabelInfoBlock infoPanelContent index =
     Html.div
         [ Attributes.css
             [ Css.padding2 (rem 0.75) (rem 0.5)
-            , Css.fontFamilies [ "Nordea Sans Small" ]
             , pseudoClass "not(:last-child)" [ Css.borderBottom3 (rem 0.0625) Css.solid Colors.mediumGray ]
-            , Css.firstChild [ Css.fontWeight (int 500), Css.fontSize (rem 1.125) ]
             ]
         ]
-        [ Html.div
-            [ Attributes.css
-                [ Css.fontSize (rem 0.875)
-                , Css.fontWeight (int 200)
-                , Css.marginBottom (rem 0.25)
-                ]
-            ]
-            [ Html.text infoPanelContent.label ]
-        , Html.text infoPanelContent.info
+        [ Text.textSmallLight |> Text.withHtmlTag Html.dt |> Text.view [] [ Html.text infoPanelContent.label ]
+        , Text.bodyTextSmall
+            |> Text.withHtmlTag Html.dd
+            |> Text.view [ Attributes.css [ Css.fontWeight (int 500), Css.fontSize (rem 1.125) ] |> attrIf (index == 0) ]
+                [ Html.text infoPanelContent.info ]
         ]
